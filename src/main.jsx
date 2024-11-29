5f4e155fb837ed9721c9160e5c0c58c977259202
Alexandre Le Sauze <alexandre.lesauze83@gmail.com>
2024-11-29 13:58:16 +0100

    debut projet 7

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './styles/index.scss'
+import Home from './pages/Home'
+import About from './pages/About'
+import Error from './pages/Error'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </StrictMode>,
 )
